9b885241b275b2a0c81dc636f9b3bf093ac43e0d
Sneha Sinha <sneha16098@iiitd.ac.in>
2017-09-29 21:12:27 +0530

    Add files via upload

--- lab7v1.java
+++ lab7v1.java
@@ -1,5 +1,6 @@
 package lab7v1;
 
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -13,40 +14,47 @@ import java.util.Scanner;
 
 //sneha sinha 2016098
 public class lab7v1 {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException, ClassNotFoundException{
 		System.out.println("Enter name of the music playlist");
 		Scanner in = new Scanner(System.in);
 		String playl = in.next();
-		File f = new File("//Users//snehasi//eclipse-workspace//lab7v1//"+playl+".txt//");
-		Scanner n = new Scanner(f);	
+		
 		//System.out.println(n.next());
 		//correct music playlist being read
 		
+		Song s1 = new Song("Sinha","ttt",5);
+		Song s2 = new Song("Sneha","tt",4);
 		//make arraylist playlist
-		String name_playlist = n.next();
+		
 		ArrayList<Song> playlist = new ArrayList<Song>();
-		int num_songs = n.nextInt();
-		System.out.println("number of songs "+num_songs);
-		for(int i=0;i<num_songs;i++) {
-			int ind = n.nextInt();
-			String na = n.next();
-			String ar = n.next();
-			Song s = new Song(na,ar,ind);
-			playlist.add(s);
-			
-		}
+		playlist.add(s1);
+		playlist.add(s2);
+		serialize(playlist);
+		
+		System.out.println("number of songs "+playlist.size());
+		
 		//menu
 		//add method
-		menu(playlist);	
+		//menu(playlist);	
+		//
+		ArrayList<Song> playlist1= new ArrayList<Song>();
+		playlist1 =deserialize(playl);
+		//for(Song s0: playlist1)
+//		{
+//			System.out.println(s0.getsong());
+//		}
+		menu(playlist);
+		//
+		
 	}
 	
-	
-	public static void serialize(String playl) throws IOException {
-		Song s1 = new Song("Amy","Britney",9);
+	public static void serialize(ArrayList<Song> playl) throws IOException {
+		
 		ObjectOutputStream out = null;
 		try {
-			out= new ObjectOutputStream(new FileOutputStream(playl+".txt"));
-			out.writeObject(s1);
+			out= new ObjectOutputStream(new FileOutputStream("//Users//snehasi//eclipse-workspace//lab7v1//playlist.txt//"));
+			for(Song s: playl)
+			    out.writeObject(s);
 		}
 		finally {
 			out.close();
@@ -54,19 +62,32 @@ public class lab7v1 {
 	}
 	
 	
-	public static void deserialize(String playl) throws FileNotFoundException, IOException, ClassNotFoundException {
-		ObjectInputStream in=null;
+	public static ArrayList<Song> deserialize(String playl) throws FileNotFoundException, IOException, ClassNotFoundException {
+		ObjectInputStream innn=null;
+		ArrayList<Song> playlist = new ArrayList<Song>();
 		try {
-			in=new ObjectInputStream(new FileInputStream("out.txt"));
-			Song s1 = (Song) in.readObject();
+			
+			innn=new ObjectInputStream(new FileInputStream("//Users//snehasi//eclipse-workspace//lab7v1//"+"playlist"+".txt//"));
+			Song s1 = (Song)innn.readObject();
+			playlist.add(s1);
+			while(s1!=null)
+			{
+				s1 = (Song) innn.readObject();
+				playlist.add(s1);
+			}
+		}
+		catch(EOFException a)
+		{
+			return playlist;
 		}
 		finally {
-			in.close();
+			innn.close();
 		}
+		return playlist;
 	}
 	
 	
-	public static void menu(ArrayList<Song> playlist) throws IOException {
+	public static void menu(ArrayList<Song> playlist) throws IOException, ClassNotFoundException {
 		Scanner in = new Scanner(System.in);
 		System.out.println("enter 1-add/2-delete/3-search/4-show/5-back and show playlists/6-exit");
 		int option= in.nextInt();
@@ -85,25 +106,9 @@ public class lab7v1 {
 			    		}
 			    }
 			    String playl2 = in.next();
-			    File f2 = new File("//Users//snehasi//eclipse-workspace//lab7v1//"+playl2+".txt//");
-				Scanner n2 = new Scanner(f2);	
-				//System.out.println(n.next());
-				//correct music playlist being read
-				
-				//make arraylist playlist
-				String name_playlist = n2.next();
-				ArrayList<Song> playlist2 = new ArrayList<Song>();
-				int num_songs2 = n2.nextInt();
-				System.out.println("number of songs "+num_songs2);
-				for(int i=0;i<num_songs2;i++) {
-					int ind2 = n2.nextInt();
-					String na2 = n2.next();
-					String ar2 = n2.next();
-					Song s2 = new Song(na2,ar2,ind2);
-					playlist.add(s2);
-					
-				}
-			    menu(playlist2);
+			    deserialize(playl2);
+			    main(null);
+			   
 			}
 			
 			else if(option==4) {
@@ -147,6 +152,7 @@ public class lab7v1 {
 			}
 			else if(option==2) {
 				//delete a song from the playlist
+				int x=0;
 				String sear = in.next();
 				int c = playlist.size();
 				if(c==0) {
@@ -158,16 +164,25 @@ public class lab7v1 {
 						//System.out.println(s.getsong());
 						if(s.getsong().equals(sear)) {
 							playlist.remove(s);
-							System.out.println(playlist.size());
+							x=playlist.size();
 							break;
 						}
 						else {
-							System.out.println("No Song Exist");
-							break;
+							x=-3;
+							//break;
 						}
 					}
+					if(x<0) {
+						System.out.println("No Song Exist");
+					}
+					else {
+						System.out.println(x);
+					}
+					
 				}
+				serialize(playlist);
 				menu(playlist);
+				
 				
 			}
 			else if(option ==1) {
@@ -178,7 +193,9 @@ public class lab7v1 {
 				Song f = new Song(see,seeartist,seedurat);
 				playlist.add(f);
 				System.out.println(playlist.size());	
+				serialize(playlist);
 				menu(playlist);
+				
 			}
 		}
 	
